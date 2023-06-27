a9cba473bc95c8e2da720c3df1074bc540b44f2b
ogriga01 <oskars.grigals@ast.lv>
2023-06-27 16:13:21 +0300

    Code clean up and optimization

--- acc_convertor_to_excel.py
+++ acc_convertor_to_excel.py
@@ -1,6 +1,6 @@
 ﻿#-------------------------------------------------------------------------------
 # Name:        PSS/E ACC to Excel
-# Version:     0.3
+# Version:     0.4
 # Purpose:
 #
 # Author:      oskars-g
@@ -10,28 +10,38 @@
 # Licence:     Free to modify and use
 #-------------------------------------------------------------------------------
 
-from tkinter import *
+from tkinter import Tk, messagebox
 from tkinter.filedialog import askopenfilename
 import os, sys
-PSSE_PATH = r'C:\Program Files\PTI\PSSE35\35.3\PSSBIN'
-sys.path.append(PSSE_PATH)
-os.environ['PATH'] += ';' + PSSE_PATH
 import psse3503
 import psspy
 import pssexcel
+
+PSSE_PATH = r'C:\Program Files\PTI\PSSE35\35.3\PSSBIN'
+sys.path.append(PSSE_PATH)
+os.environ['PATH'] += ';' + PSSE_PATH
+
 psspy.psseinit(7000)
 
 def select_file(file_type='.acc', dialogue_title="Select file"):
-    """ Single file selection popup
-    return: list"""
-    Tk().withdraw()
-    filename = askopenfilename(filetypes=[('{} file'.format(file_type), '*{}'.format(
-        file_type))])
-    return [filename]
+    """ Single file selection popup"""
+    #Tk().withdraw()
+    root = Tk()
+    root.withdraw()
+    filename = askopenfilename(filetypes=[('{} file'.format(file_type), '*{}'.format(file_type))],
+                               title=dialogue_title)
+    return filename
 
 if __name__ == "__main__":
     data_file = select_file()
-    acc_data = data_file[0]
-    pssexcel.accc(acc_data, ['s', 'e', 'v', 'b', 'l', 'g'], ratecon='a',  # export a summary
+    if data_file:
+        acc_data = data_file
+        pssexcel.accc(acc_data, ['s', 'e', 'v', 'b', 'l', 'g'], ratecon='a',  # export a summary
                     xlsfile= 'Contingency_Report.xls', overwritesheet=True, show=False)  # export to this file
+
+        messagebox.showinfo("Process Completed", "Contingency report generated successfully.")
+    else:
+        messagebox.showwarning("No File Selected", "No file was selected. The process will now end.")
+        sys.exit(0)
+
 
